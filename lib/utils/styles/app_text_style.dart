151c1f05ded5557e18196e9aa9628a5a65bac2b4
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-02-17 06:48:48 +0500

    First ResultScreen

--- lib/utils/styles/app_text_style.dart
+++ lib/utils/styles/app_text_style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_quiz_app/utils/colors/app_colors.dart';
 
 class AppTextStyle {
   //Inter
@@ -6,39 +7,46 @@ class AppTextStyle {
     fontFamily: "Inter",
     fontWeight: FontWeight.w200,
     fontSize: 14,
+    color: AppColors.cF2F2F2,
   );
   static const TextStyle interLight = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w300,
     fontSize: 14,
+    color: AppColors.cF2F2F2,
   );
   static const TextStyle interRegular = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w400,
     fontSize: 16,
+    color: AppColors.cF2F2F2,
   );
   static const TextStyle interMedium = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w500,
     fontSize: 14,
+    color: AppColors.cF2F2F2,
   );
 
   static const TextStyle interSemiBold = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w600,
     fontSize: 16,
+    color: AppColors.cF2F2F2,
   );
 
   static const TextStyle interBold = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w700,
     fontSize: 16,
+    color: AppColors.cF2F2F2,
   );
 
   static const TextStyle interBlack = TextStyle(
     fontFamily: "Inter",
     fontWeight: FontWeight.w800,
     fontSize: 16,
+    color: AppColors.cF2F2F2,
   );
 
   //Bodoni
@@ -46,11 +54,13 @@ class AppTextStyle {
     fontFamily: "Bodoni",
     fontWeight: FontWeight.w700,
     fontSize: 14,
+    color: AppColors.cF2F2F2,
   );
 
   static const TextStyle bodoniRegular = TextStyle(
     fontFamily: "Bodoni",
     fontWeight: FontWeight.w400,
     fontSize: 16,
+    color: AppColors.cF2F2F2,
   );
 }
