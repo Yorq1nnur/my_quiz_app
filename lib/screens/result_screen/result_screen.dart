2234e2a676f951f1007253e899862f4de6f71e1a
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-02-17 07:05:11 +0500

    First ResultScreen

--- lib/screens/result_screen/result_screen.dart
+++ lib/screens/result_screen/result_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -244,96 +245,96 @@ class _ResultScreenState extends State<ResultScreen> {
                 ],
               ),
             ),
-            SizedBox(
-              height: 30.h,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(
-                vertical: 15.h,
-                horizontal: 32.w,
-              ),
-              decoration: BoxDecoration(
-                color: AppColors.c162023,
-                borderRadius: BorderRadius.circular(
-                  40.r,
+            const Spacer(),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 15.h,
+                  horizontal: 32.w,
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: AppColors.c000000.withOpacity(
-                      0.25.r,
-                    ),
-                    blurRadius: 4.r,
-                    offset: Offset(
-                      0,
-                      4.r,
-                    ),
+                decoration: BoxDecoration(
+                  color: AppColors.c162023,
+                  borderRadius: BorderRadius.circular(
+                    40.r,
                   ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  RichText(
-                    text: TextSpan(
-                        text: "Next:\n",
-                        style: AppTextStyle.interBold.copyWith(
-                          color: AppColors.cF2F2F2,
-                          fontSize: 15.sp,
-                          fontWeight: FontWeight.w400,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: "Quadratic Equations",
-                            style: AppTextStyle.interBold.copyWith(
-                              color: AppColors.cF2F2F2,
-                              fontSize: 15.sp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          )
-                        ]),
-                  ),
-                  ZoomTapAnimation(
-                    onTap: () {},
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 8.h,
-                        horizontal: 20.w,
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.c000000.withOpacity(
+                        0.25.r,
                       ),
-                      decoration: BoxDecoration(
-                        color: AppColors.cF2954D,
-                        boxShadow: [
-                          BoxShadow(
-                            color: AppColors.c000000.withOpacity(
-                              0.25,
-                            ),
-                            blurRadius: 12.r,
-                            offset: Offset(
-                              0,
-                              5.r,
-                            ),
-                          ),
-                        ],
-                        border: Border.all(
-                          color: AppColors.cF2954D,
-                          width: 1.w,
-                        ),
-                        borderRadius: BorderRadius.circular(
-                          16.r,
-                        ),
+                      blurRadius: 4.r,
+                      offset: Offset(
+                        0,
+                        4.r,
                       ),
-                      child: Center(
-                        child: Text(
-                          "Go",
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                          text: "Next:\n",
                           style: AppTextStyle.interBold.copyWith(
                             color: AppColors.cF2F2F2,
-                            fontSize: 16.sp,
-                            fontWeight: FontWeight.w600,
+                            fontSize: 15.sp,
+                            fontWeight: FontWeight.w400,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: "Quadratic Equations",
+                              style: AppTextStyle.interBold.copyWith(
+                                color: AppColors.cF2F2F2,
+                                fontSize: 15.sp,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          ]),
+                    ),
+                    ZoomTapAnimation(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 8.h,
+                          horizontal: 20.w,
+                        ),
+                        decoration: BoxDecoration(
+                          color: AppColors.cF2954D,
+                          boxShadow: [
+                            BoxShadow(
+                              color: AppColors.c000000.withOpacity(
+                                0.25,
+                              ),
+                              blurRadius: 12.r,
+                              offset: Offset(
+                                0,
+                                5.r,
+                              ),
+                            ),
+                          ],
+                          border: Border.all(
+                            color: AppColors.cF2954D,
+                            width: 1.w,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                            16.r,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Go",
+                            style: AppTextStyle.interBold.copyWith(
+                              color: AppColors.cF2F2F2,
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
