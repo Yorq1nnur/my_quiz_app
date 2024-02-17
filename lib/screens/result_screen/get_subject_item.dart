b79744c6f6261b87115b6628cf6f9d39f9a5603b
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-02-17 05:38:37 +0500

    First ResultScreen

---
+++ lib/screens/result_screen/get_subject_item.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../utils/colors/app_colors.dart';
+import '../../utils/styles/app_text_style.dart';
+
+class GetSubjectItem extends StatelessWidget {
+  const GetSubjectItem({super.key, required this.subjectName, required this.subjectDescription});
+
+  final String subjectName;
+  final String subjectDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 20.h,
+      ),
+      padding: EdgeInsets.symmetric(
+        vertical: 16.h,
+        horizontal: 20.w,
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.c162023,
+        border: Border.all(
+          color: AppColors.c2F3739,
+          width: 1.w,
+        ),
+        borderRadius: BorderRadius.circular(
+          16.r,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.c000000.withOpacity(0.25),
+            blurRadius: 12.r,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            subjectName,
+            style: AppTextStyle.interBold.copyWith(
+              color: AppColors.cF2F2F2,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 8.h,),
+          Text(subjectDescription, style: AppTextStyle.interBold.copyWith(
+              color: AppColors.cF2F2F2.withOpacity(0.5),
+              fontSize: 13.sp,
+              fontWeight: FontWeight.w400
+          ),)
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/result_screen/result_screen.dart
+++ lib/screens/result_screen/result_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:my_quiz_app/models/subject_model.dart';
+import 'package:my_quiz_app/screens/result_screen/get_subject_item.dart';
 import 'package:my_quiz_app/subject_screen/subject_screen.dart';
 import 'package:my_quiz_app/utils/colors/app_colors.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
@@ -10,7 +11,10 @@ import '../../utils/images/app_images.dart';
 import '../../utils/styles/app_text_style.dart';
 
 class ResultScreen extends StatefulWidget {
-  const ResultScreen({super.key, required this.subjectModel,});
+  const ResultScreen({
+    super.key,
+    required this.subjectModel,
+  });
 
   final SubjectModel subjectModel;
 
@@ -21,108 +25,177 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion(value: const SystemUiOverlayStyle(
-      statusBarColor: AppColors.transparent,
-    ), child: Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(top: 50.h, left: 20.w, right: 20.w,),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                left: 32.w,
-                right: 32.w,
-                top: 50.h,
-                bottom: 22.h,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  ZoomTapAnimation(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                          context, MaterialPageRoute(builder: (context) =>
-                          const SubjectScreen()));
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: AppColors.c162023,
-                        borderRadius: BorderRadius.circular(8),
-                        boxShadow: [
-                          const BoxShadow(
-                              color: AppColors.c2F3739, spreadRadius: 1),
-                          BoxShadow(
-                            color: AppColors.c000000.withOpacity(0.25),
-                            blurRadius: 12,
-                            offset: Offset(
-                              0,
-                              5.w,
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: AppColors.transparent,
+      ),
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(
+            top: 50.h,
+            left: 35.w,
+            right: 35.w,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 22.h,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ZoomTapAnimation(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SubjectScreen()));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.c162023,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            const BoxShadow(
+                                color: AppColors.c2F3739, spreadRadius: 1),
+                            BoxShadow(
+                              color: AppColors.c000000.withOpacity(0.25),
+                              blurRadius: 12,
+                              offset: Offset(
+                                0,
+                                5.w,
+                              ),
+                            ),
+                          ],
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 12.w, vertical: 12.w),
+                        child: Center(
+                          child: SvgPicture.asset(
+                            AppImages.arrowBack,
+                            height: 24.h,
+                            width: 24.w,
+                            fit: BoxFit.cover,
+                            colorFilter: const ColorFilter.mode(
+                              AppColors.cF2F2F2,
+                              BlendMode.srcIn,
                             ),
                           ),
-                        ],
-                      ),
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 12.w, vertical: 12.w),
-                      child: Center(
-                        child: SvgPicture.asset(
-                          AppImages.arrowBack,
-                          height: 24.h,
-                          width: 24.w,
-                          fit: BoxFit.cover,
-                          colorFilter: const ColorFilter.mode(
-                            AppColors.cF2F2F2,
-                            BlendMode.srcIn,
-                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 30.w,
-                  ),
-                  Text(
-                    "Natija",
-                    style: AppTextStyle.interBold.copyWith(
-                      color: AppColors.cF2F2F2,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 20.sp,
+                    SizedBox(
+                      width: 30.w,
                     ),
-                  ),
-                  const Spacer(),
-                  ZoomTapAnimation(
-                    onTap: () {},
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 12.h,
-                        horizontal: 15.w,
-                      ),
-                      decoration: BoxDecoration(
-                        color: AppColors.c273032,
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: AppColors.cF2954D,
-                            spreadRadius: 2,
-                          ),
-                        ],
-                      ),
-                      child: Text(
-                        "Yuborish",
-                        style: AppTextStyle.interThin.copyWith(
-                          color: AppColors.cF2F2F2,
-                          fontSize: 16.sp,
-                          fontWeight: FontWeight.w600,
-                        ),
+                    Text(
+                      "Natija",
+                      style: AppTextStyle.interBold.copyWith(
+                        color: AppColors.cF2F2F2,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20.sp,
                       ),
                     ),
-                  )
-                ],
+                  ],
+                ),
               ),
-            )
-          ],
+              GetSubjectItem(
+                subjectName: widget.subjectModel.subjectName,
+                subjectDescription: widget.subjectModel.description,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 50.w,
+                  vertical: 20.h,
+                ),
+                margin: EdgeInsets.only(
+                  bottom: 20.h,
+                ),
+                decoration: BoxDecoration(
+                  color: AppColors.c162023,
+                  border: Border.all(
+                    color: AppColors.c2F3739,
+                    width: 1.w,
+                  ),
+                  borderRadius: BorderRadius.circular(
+                    16.r,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColors.c000000.withOpacity(0.25),
+                      blurRadius: 12.r,
+                      offset: Offset(
+                        0,
+                        5.r,
+                      ),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircularProgressIndicator(
+                      color: AppColors.cF2954D,
+                      value: 0.7,
+                      strokeWidth: 10.w,
+                      strokeAlign: BorderSide.strokeAlignOutside,
+                      strokeCap: StrokeCap.butt,
+                      valueColor: const AlwaysStoppedAnimation<Color>(
+                          AppColors.cF2954D),
+                      backgroundColor: AppColors.cF2954D.withOpacity(0.5),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 150.w,
+                          child: RichText(
+                            text: TextSpan(
+                              text: "Congratulations!You have",
+                              style: AppTextStyle.interBold.copyWith(
+                                color: AppColors.cF2F2F2,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.w700,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: " passed",
+                                  style: AppTextStyle.interBold.copyWith(
+                                    color: AppColors.c6FCF97,
+                                    fontSize: 16.sp,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                 TextSpan(
+                                   text: " this test with ",
+                                   style: AppTextStyle.interBold.copyWith(
+                                     color: AppColors.cF2F2F2,
+                                     fontSize: 16.sp,
+                                     fontWeight: FontWeight.w700,
+                                   ),
+                                 ),
+                                 TextSpan(
+                                   text: " 80%.",
+                                   style: AppTextStyle.interBold.copyWith(
+                                     color: AppColors.cF2954D,
+                                     fontSize: 16.sp,
+                                     fontWeight: FontWeight.w700,
+                                   ),
+                                 ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
